6961cbd3103daf7f6c953102ff87e3e501a895ea
aent <alintssr@gmail.com>
2024-02-05 13:14:51 -0500

    react-state-exercises

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import ScoreKeeper from './ScoreKeeper';
+import IconList from './IconList';
+import Ball from './Ball';
+import Lottery from './Lottery';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ScoreKeeper /> */}
+      {/* <IconList/> */}
+      {/* <Ball num={17} />
+      <Ball num={13} />
+      <Ball num={11} />
+      <Ball num={9} /> */}
+      <Lottery />
+      <Lottery title='Mini Daily' maxNum={10} numBalls={4} />
     </div>
   );
 }
